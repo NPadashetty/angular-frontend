8a812cd985e93e6eb2a4a2fc08bcc68d677a23ab
nikhilesh <nikhileshg.padashetty@gmail.com>
2023-12-02 18:12:43 +0530

    button update

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,7 +1,10 @@
+// employee-list.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -9,23 +12,28 @@ import { Router } from '@angular/router';
 })
 export class EmployeeListComponent implements OnInit {
   employees!: Employee[];
-  constructor(private employeeService: EmployeeService,
-    private router: Router) { }
+
+  constructor(private employeeService: EmployeeService, private router: Router) {}
+
   ngOnInit(): void {
     this.getEmployees();
   }
+
   private getEmployees() {
     this.employeeService.getEmployeeList().subscribe(data => {
       console.log(data);
       this.employees = data;
     });
   }
+
   employeeDetails(id: number) {
     this.router.navigate(['employee-details', id]);
   }
-  UpdateEmployeeComponent(id: number) {
+
+  updateEmployee(id: number) { // Rename the method to updateEmployee
     this.router.navigate(['update-employee', id]);
   }
+
   deleteEmployee(id: number) {
     this.employeeService.deleteEmployee(id).subscribe(data => {
       console.log(data);
